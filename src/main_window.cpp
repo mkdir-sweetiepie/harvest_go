56023e59a2b4012408f0eda8cf3b17af552552ed
Hong Jihyeon <abab4551817@kw.ac.kr>
2025-06-30 00:46:51 +0900

    edit fullname command

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -8,7 +8,7 @@
 /*****************************************************************************
 ** Includes
 *****************************************************************************/
-#include "../include/harvest_go/main_window.hpp"
+#include "../include/turtle_go/main_window.hpp"
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindowDesign) {
   ui->setupUi(this);
@@ -22,7 +22,7 @@ void MainWindow::closeEvent(QCloseEvent* event) { QMainWindow::closeEvent(event)
 MainWindow::~MainWindow() { delete ui; }
 
 void MainWindow::on_On_clicked() {
-  // 1. RealSense 카메라 실행 (cam alias 명령어)
+  // 1. RealSense 카메라 실행
   system(
       "gnome-terminal --geometry=80x18+0+0 -- bash -c '"
       "ros2 launch realsense2_camera rs_launch.py "
@@ -35,26 +35,24 @@ void MainWindow::on_On_clicked() {
       "enable_sync:=true "
       "color_qos:=SENSOR_DATA "
       "depth_qos:=SENSOR_DATA; exec bash'");
-  std::this_thread::sleep_for(std::chrono::seconds(2));
+  std::this_thread::sleep_for(std::chrono::seconds(3));
 
   // 2. YOLO 실행 (yolo12_bbox_coordinate)
   system(
       "gnome-terminal --geometry=80x18+800+0 -- bash -c '"
       "ros2 run yolo12_bbox_coordinate yolo_XYZ_pub_comp; exec bash'");
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::this_thread::sleep_for(std::chrono::seconds(2));
 
-  // 3. FoundationPose 실행 (별도 터미널에서 실행)
+  // 3. FoundationPose 실행
   system(
       "gnome-terminal --geometry=80x18+0+300 -- bash -c '"
-      "run_foundation; exec bash'");
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-
-  // 4. YOLOv8 detection launch (기존 코드)
-  system(
-      "gnome-terminal --geometry=80x18+800+300 -- bash -c '"
-      "ros2 launch yolov8_detection yolov8_detection_launch.py; exec bash'");
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-
+      "source ~/miniconda3/etc/profile.d/conda.sh && "
+      "conda activate foundationpose_ros && "
+      "export PYTHONNOUSERSITE=True && "
+      "export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 && "
+      "cd ~/FoundationPoseROS2 && "
+      "python foundationpose_ros_multi.py; exec bash'");
+  std::this_thread::sleep_for(std::chrono::seconds(2));
 }
 
 void MainWindow::on_Off_clicked() {
@@ -63,6 +61,7 @@ void MainWindow::on_Off_clicked() {
   system("pkill -f 'ros2 run'");
   system("pkill -f 'realsense2_camera'");
   system("pkill -f 'yolo12_bbox_coordinate'");
-  system("pkill -f 'yolov8_detection'");
-  system("pkill -f 'run_foundation'");
+  system("pkill -f 'foundationpose_ros_multi.py'");
+  system("pkill -f 'python foundationpose_ros_multi.py'");
+  system("pkill -f 'conda activate foundationpose_ros'");
 }
